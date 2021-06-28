3d1b343058b2373d0ad95f504b97f740682f2690
ricardomilhazesm <rmilhazesveloso@gmail.com>
2021-06-28 17:06:35 +0100

    Code formatting and clearing outputs

--- opinion_mining/phrase_breaker.py
+++ opinion_mining/phrase_breaker.py
@@ -1,49 +1,57 @@
 import string
 
-#Converts the string to the automata alphabet
+# Converts the string to the automata alphabet
 def convert_automata_alphabet(c):
-    ax = ''
-    if(c.isalpha() or c.isnumeric()):
-        if(c in string.ascii_lowercase):
-            ax = 'x'
-        else: 
-            ax = 'X'
+    ax = ""
+    if c.isalpha() or c.isnumeric():
+        if c in string.ascii_lowercase:
+            ax = "x"
+        else:
+            ax = "X"
     else:
-        ax = '.'
+        ax = "."
     return ax
 
-def word_breaker(transitions,initial,accepting,s):
+
+def word_breaker(transitions, initial, accepting, s):
     state = initial
     state_b = initial
-    ns = ''
+    ns = ""
     for c in s:
         ax = convert_automata_alphabet(c)
         state_b = state
         state = transitions[state][ax]
-        #print(state)
-        if( (state_b == 3 and state == 2) or (state_b == 3 and state == 1)
-         or (state_b == 2 and state == 1) or (state_b == 4 and state == 1) or
-         (state_b == 4 and state == 2)):
-            c = ' '+c
-        ns = ns+c
+        # print(state)
+        if (
+            (state_b == 3 and state == 2)
+            or (state_b == 3 and state == 1)
+            or (state_b == 2 and state == 1)
+            or (state_b == 4 and state == 1)
+            or (state_b == 4 and state == 2)
+        ):
+            c = " " + c
+        ns = ns + c
 
-    #return ns, state in accepting
+    # return ns, state in accepting
     return ns
 
+
 def phrase_breaker(t):
-	#automata definition
-	dfa = {0:{'X':1, 'x':2, '.':4},
-       	   1:{'X':1,'x':2, '.':3},
-           2:{'x':2, 'X':1, '.':3},
-       	   3:{'X':1, 'x':2, '.':5},
-       	   4:{'X':1, 'x':2, '.':5},
-           5:{'.':5, 'x':5, 'X': 5}}
-    #split the phrase in words 
-	l = t.split()
-	li =[]
-	s = " "
-	#apply the word breaker 
-	for i in range(len(l)):
-		li.append(word_breaker(dfa,0,{1,2,3,4},l[i]))
-		s=" ".join(li)
-	return s
+    # automata definition
+    dfa = {
+        0: {"X": 1, "x": 2, ".": 4},
+        1: {"X": 1, "x": 2, ".": 3},
+        2: {"x": 2, "X": 1, ".": 3},
+        3: {"X": 1, "x": 2, ".": 5},
+        4: {"X": 1, "x": 2, ".": 5},
+        5: {".": 5, "x": 5, "X": 5},
+    }
+    # split the phrase in words
+    l = t.split()
+    li = []
+    s = " "
+    # apply the word breaker
+    for i in range(len(l)):
+        li.append(word_breaker(dfa, 0, {1, 2, 3, 4}, l[i]))
+        s = " ".join(li)
+    return s
--- opinion_mining/web_scrapper.py
+++ opinion_mining/web_scrapper.py
@@ -8,11 +8,11 @@ n_pages = 0
 URL = "https://pt.coursera.org/learn/machine-learning/reviews?"
 
 polarity = {
-    '1': 'negative',
-    '2': 'negative',
-    '3': 'neutral',
-    '4': 'positive',
-    '5': 'positive',
+    "1": "negative",
+    "2": "negative",
+    "3": "neutral",
+    "4": "positive",
+    "5": "positive",
 }
 
 for n_star in range(1, 6):
@@ -24,22 +24,22 @@ for n_star in range(1, 6):
     elif n_star == 3:
         n_pages = 12
     elif n_star == 4:
-        n_pages = 105 # 105
+        n_pages = 105  # 105
     elif n_star == 5:
-        n_pages = 400 # 400
-    
+        n_pages = 400  # 400
+
     for n_page in range(1, n_pages):
         page = requests.get(URL + "star=" + str(n_star) + "&page=" + str(n_page))
         soup = BeautifulSoup(page.content, "html.parser")
         results = list(soup.find_all("div", attrs={"class": "review"}))
-            
+
         for result in results:
             rev_wrap = result.contents[0]
             rev_text = rev_wrap.find_all("div", attrs={"class": "reviewText"})[0].text
-            if detect(rev_text) == 'en':
+            if detect(rev_text) == "en":
                 review = [rev_text, n_star, polarity[str(n_star)]]
                 reviews.append(review)
 
-with open('reviews.csv', 'w', encoding='utf8') as file:
+with open("reviews.csv", "w", encoding="utf8") as file:
     writer = csv.writer(file)
     writer.writerows(reviews)
